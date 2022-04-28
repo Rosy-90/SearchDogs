6aa0d02fcd0813801aa5f630457ea93c37caa8cb
Rosy Patel <rosypatel109@hotmail.com>
2022-04-28 13:17:46 +0100

    initial set up for storyboard and added pet details view controller

---
+++ SearchDogs/View/PetDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PetDetailsViewController.swift
+//  SearchDogs
+//
+//  Created by Rosy Patel on 28/04/2022.
+//
+
+import Foundation
+import UIKit
+
+class PetDetailsViewController: UIViewController {
+    
+}
--- SearchDogs/View/PetsViewController.swift
+++ SearchDogs/View/PetsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PetsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
