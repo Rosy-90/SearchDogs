f4ed1999eec5bbbd62474c9f477bd5efaed71a99
Rosy Patel <rosypatel109@hotmail.com>
2022-05-03 18:05:06 +0100

    deleted pod lock file and pod folder

--- Pods/Target Support Files/Pods-SearchDogs-SearchDogsUITests/Pods-SearchDogs-SearchDogsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SearchDogs_SearchDogsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SearchDogs_SearchDogsUITests
-@end
--- Pods/Target Support Files/Pods-SearchDogsTests/Pods-SearchDogsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SearchDogsTests : NSObject
-@end
-@implementation PodsDummy_Pods_SearchDogsTests
-@end
