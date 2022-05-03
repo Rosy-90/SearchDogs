b212a6efe933aec62e55686adace73577562a586
Rosy Patel <rosypatel109@hotmail.com>
2022-05-03 16:55:55 +0100

    removes kingfisfer methods and implements downloadImage method

--- SearchDogs/View/PetsViewCell.swift
+++ SearchDogs/View/PetsViewCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 class PetsViewCell: UICollectionViewCell {
     
@@ -19,6 +18,43 @@ class PetsViewCell: UICollectionViewCell {
     }
     func setPetImage(imageName: String) {
         let url = URL(string: imageName)
-        petImageView.kf.setImage(with: url)
+        downloadDogsImagesForSearchResult(imageURL: url)
+        
+    }
+    
+    func downloadDogsImagesForSearchResult(imageURL: URL?) {
+        let imageCache = NSCache<AnyObject, AnyObject>.sharedInstance
+        let downloadTask: URLSessionDownloadTask?
+        
+        if let urlOfImage = imageURL {
+            if let cachedImage = imageCache.object(forKey: urlOfImage.absoluteString as NSString) {
+                self.petImageView.image = cachedImage as? UIImage
+            } else {
+                let session = URLSession.shared
+                downloadTask = session.downloadTask(
+                    with: urlOfImage as URL, completionHandler: { [weak self] url, response, error in
+                        if error == nil, let url = url, let data = NSData(contentsOf: url), let image = UIImage(data: data as Data) {
+                            
+                            DispatchQueue.main.async() {
+                                let imageToCache = image
+                                if let self = self, let imageView = self.petImageView {
+                                    imageView.image = imageToCache
+                                    imageCache.setObject(imageToCache, forKey: urlOfImage.absoluteString as NSString , cost: 1)
+                                }
+                            }
+                        } else {
+                            print("ERROR \(String(describing: error?.localizedDescription))")
+                        }
+                    })
+                downloadTask!.resume()
+            }
+        }
+    }
+}
+
+extension NSCache {
+    @objc class var sharedInstance: NSCache<NSString, AnyObject> {
+        let cache = NSCache<NSString, AnyObject>()
+        return cache
     }
 }
